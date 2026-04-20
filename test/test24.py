89ffc3de7c2ec2742b45c309769c29cb65a1c58b
zhai-zhaobo <1068086254@qq.com>
2026-04-20 20:00:16 +0800

    11

--- test/test24.py
+++ test/test24.py
@@ -3,8 +3,8 @@
 # c=float(input("输入第三个数"))
 # max_num=max(a,b,c)
 # print(f"输出的最大数为：{max_num}")
-# from math import sqrt
-
+from math import sqrt
+#
 # year=int(input("请输入一个年份" ))
 #
 # if (year%4==0)and (year%4!=0) or(year%400==0):
@@ -32,7 +32,7 @@
 #     height=(father_height+mother_height)/2
 # else:
 #     print("性别不符合要求，无对应公式")
-# print(f"你的身高为： {height}cm")
+# print(f"你的身高为： {height:.2f}cm")
 
 # height=float(input("请输入你的身高 "))#cm
 # weight=float(input("请输入你的体重 "))#kg
