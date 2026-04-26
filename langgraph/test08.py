8da4f552a9b65c13edc3592adffabdfef93bff89
zhai-zhaobo <1068086254@qq.com>
2026-04-26 21:55:16 +0800

    持久化

--- langgraph/test08.py
+++ langgraph/test08.py
@@ -108,28 +108,47 @@ agent_builder.add_edge("tool_node","llm_call")
 # ⽰
 # checkpointer=InMemorySaver()
 # agent_search=agent_builder.compile(checkpointer=checkpointer)
-DB_URI="postgresql://postgres:bit@localhost:5432/postgres"
-with PostgresSaver.from_conn_string(DB_URI) as checkpointer:
-    # 第一次调用Postgres需要调用checkpointer.setup()
-    checkpointer.setup()
-    agent_search=agent_builder.compile(checkpointer=checkpointer)
-    config={"configurable":{"thread_id":"1"}}
-    result = agent_search.invoke(
-        {"messages": [HumanMessage(content="今天烟台天气怎么样")]},
-        config
-    )
-    print(f"调用LLM的次数:{result['llm_calls']}次")
-    for m in result["messages"]:
-        m.pretty_print()
-    # ----------------------------------------------------------------------------
-    config = {"configurable": {"thread_id": "1"}}
-    result = agent_search.invoke(
-        {"messages": [HumanMessage(content="我刚才问的什么")]},
-        config
-    )
-    print(f"调用LLM的次数:{result['llm_calls']}次")
-    for m in result["messages"]:
-        m.pretty_print()
+# DB_URI="postgresql://postgres:bit@localhost:5432/postgres"
+# with PostgresSaver.from_conn_string(DB_URI) as checkpointer:
+#     # 第一次调用Postgres需要调用checkpointer.setup()
+#     checkpointer.setup()
+#     agent_search=agent_builder.compile(checkpointer=checkpointer)
+
+
+
+    # config={"configurable":{"thread_id":"1"}}
+    # 获取最新快照状态
+    # #     调用前
+    # # snapshot=agent_search.get_state(config)
+    # # print(snapshot)
+    # result = agent_search.invoke(
+    #     {"messages": [HumanMessage(content="今天烟台天气怎么样")]},
+    #     config
+    # )
+    # print(f"调用LLM的次数:{result['llm_calls']}次")
+    # for m in result["messages"]:
+    #     m.pretty_print()
+    #     调用后
+    # snapshot = agent_search.get_state(config)
+    # print(snapshot)
+# ----------------------------------------------------------------------------
+#     config = {"configurable": {"thread_id": "1"}}
+#     result1 = agent_search.invoke(
+#         {"messages": [HumanMessage(content="你好")]},
+#         config
+#     )
+#     # 查看状态历史记录
+#     history = list(agent_search.get_state_history(config))
+#     print(history)
+# ----------------------------------------------------------------------------
+    # config = {"configurable": {"thread_id": "1"}}
+    # result = agent_search.invoke(
+    #     {"messages": [HumanMessage(content="我刚才问的什么")]},
+    #     config
+    # )
+    # print(f"调用LLM的次数:{result['llm_calls']}次")
+    # for m in result["messages"]:
+    #     m.pretty_print()
 # 5.执行图
 # messages = agent_search.invoke({
 #     "messages": [
@@ -156,3 +175,47 @@ with PostgresSaver.from_conn_string(DB_URI) as checkpointer:
 #     plt.show() # 弹出窗⼝显⽰图⽚
 # except Exception as e:
 #     print(f"An error occurred: {e}")
+
+
+'''实现重放功能'''
+DB_URI="postgresql://postgres:bit@localhost:5432/postgres"
+with PostgresSaver.from_conn_string(DB_URI) as checkpointer:
+    # 第一次调用Postgres需要调用checkpointer.setup()
+    checkpointer.setup()
+    agent_search=agent_builder.compile(checkpointer=checkpointer)
+    config = {"configurable": {"thread_id": "33"}}
+    # 第⼀次执⾏
+    result1 = agent_search.invoke(
+        {"messages": [HumanMessage(content="今天烟台莱山区天气怎么样")]},
+        config
+    )
+    # 打印最新消息
+    result1["messages"][-1].pretty_print()
+
+    result1 = agent_search.invoke(
+        {"messages": [HumanMessage(content="我刚才问的什么")]},
+        config
+    )
+    # 打印全部消息
+    for r in  result1["messages"]:
+        r.pretty_print()
+
+
+
+
+    # 保存调⽤⼯具前的状态
+    # print("-" * 80)
+    # print(f"第⼀次执⾏历史：")
+    to_replay = None
+    for snapshot in agent_search.get_state_history(config):
+        print("checkpoint_id: ", snapshot.config["configurable"]["checkpoint_id"])
+        print( "下⼀节点: ", snapshot.next)
+        print("\n")
+        # 遍历所有历史 → 找到 ID  → 把它存起来，准备重新运行
+        # if snapshot.config["configurable"]["checkpoint_id"]== "1f141646-00ee-67d2-8005-1be46877f28e":
+        #     # print("state:",snapshot.values["messages"][-1])
+        #     to_replay=snapshot
+
+    # 重放（从指定的快照开始执行）
+    # result=agent_search.invoke(None,config=to_replay.config)
+    # print(result)
