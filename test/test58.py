473d74f42d013bc67a93ba4cbd55bc0d649a2f85
zhai-zhaobo <1068086254@qq.com>
2026-06-29 21:33:57 +0800

    每日算法

---
+++ test/test58.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        #全局最大和
+        max_sum=nums[0]
+        #以当前位置最大和
+        cur_sum=nums[0]
+        for i in range(1,len(nums)):
+            cur_sum=max(nums[i],cur_sum+nums[i])
+            max_sum=max(max_sum,cur_sum)
+        return max_sum
